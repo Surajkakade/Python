1844434a94b82b4fd25472b01f7a62f77957a16b
SURAJ KAKADE <79645053+Surajkakade@users.noreply.github.com>
2021-03-09 15:16:30 +0530

    Create SemScores.py

---
+++ SemScores.py
@@ -0,0 +1,16 @@
+# Python
+
+#Below are a set of scores that students have received in the past semester. Write code to determine how many are 90 or above and assign that result to the value a_scores.
+
+scores = "67 80 90 78 93 20 79 89 96 97 92 88 79 68 58 90 98 100 79 74 83 88 80 86 85 70 90 100"
+scores=scores.split()
+print(scores)
+
+a_scores=0
+
+for i in scores:
+    i=int(i)
+    if i >=90:
+        a_scores+=1
+
+print(a_scores)
